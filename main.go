a99a7b966c5ee14267dfa8a90ca26c2cae2ba42e
satou-yosiyuki <stuysyk1027@icloud.com>
2022-11-02 22:25:47 +0900

    型の学習

--- main.go
+++ main.go
@@ -8,4 +8,26 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println(time.Now())
+	var i int = 100
+	fmt.Println(i)
+	var s string = "100"
+	fmt.Println(s)
+	var t,f bool = true,false
+	fmt.Println(t,f)
+	var (
+		i2 int = 1009
+		s2 string = "golang"
+	)
+	fmt.Println(i2,s2)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3,s3)
+
+	// 暗黙的な定義
+	i4 := 400
+	fmt.Println(i4)
+
+	i4 = 450
+	fmt.Println(i4)
 }
